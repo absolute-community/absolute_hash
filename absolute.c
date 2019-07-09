6d1895c1c468b9f13356b5c31e813acbe39f52a0
Crypto Centric <cryptocentric@absolutecoin.net>
2019-07-09 10:56:50 +0100

    Include update

--- absolute.c
+++ absolute.c
@@ -4,18 +4,16 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "sha3/sph_blake.h"
-#include "sha3/sph_bmw.h"
-#include "sha3/sph_groestl.h"
-#include "sha3/sph_jh.h"
-#include "sha3/sph_keccak.h"
-#include "sha3/sph_skein.h"
-#include "sha3/sph_luffa.h"
-#include "sha3/sph_cubehash.h"
-#include "sha3/sph_shavite.h"
-#include "sha3/sph_simd.h"
-#include "sha3/sph_echo.h"
-#include "sha3/Lyra2.h"
+#include "sha3/Lyra2RE.c"
+#include "sha3/Lyra2.c"
+#include "sha3/Sponge.c"
+#include "sha3/blake.c"
+#include "sha3/bmw.c"
+#include "sha3/cubehash.c"
+#include "sha3/groestl.c"
+#include "sha3/keccak.c"
+#include "sha3/skein.c"
+#include "sha3/simd.c"
 
 
 void absolute_hash(const char* input, char* output)
